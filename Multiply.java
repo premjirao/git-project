937572d5919619b411ed705c0536cbd6ef2c3a18
premchand <premrao44@gmail.com>
2023-02-24 11:52:41 +0530

    the new file commited multiply.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String argd) {
+        int prem=12,sanjana=23;
+        System.out.println("the product is"+prem*sanjana);
+        
+    }
+    
+}
